df4a9f6e2ba3b3c641108e3cf99aafb307e0fa80
Denis Semko <53062219+DenisSemko@users.noreply.github.com>
2023-08-09 16:12:23 +0300

    Add CoinGecko client

---
+++ src/Services/CryptoAgent/CryptoAgent.CoinAPI/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using CoinGecko.Entities.Response.Coins;
+global using CoinGecko.Interfaces;
+global using Microsoft.AspNetCore.Mvc;
+global using CryptoAgent.CoinAPI.Services;
+global using System.Net;
+global using CoinGecko.Clients;
